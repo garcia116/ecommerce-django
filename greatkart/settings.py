d032eccce12acb727e14c3f9d62c54e757523f9f
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-04-20 15:39:27 -0700

    settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-2xmzmwxd.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['garcia-ecommerce-project.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
