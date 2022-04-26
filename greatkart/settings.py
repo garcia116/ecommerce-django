e451bf70711ef35db779d9a63d6ec320102cac01
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-04-25 21:09:21 -0700

    static

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -155,9 +155,9 @@ AWS_LOCATION = 'static'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATICFILES_DIRS = [
-#    'greatkart/static',
-#]
+STATICFILES_DIRS = [
+    'greatkart/static',
+]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
