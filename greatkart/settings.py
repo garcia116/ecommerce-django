9a0cc741cafd7ca967b861599b2e3f69e39475ec
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-04-25 18:33:35 -0700

    env

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -169,9 +169,9 @@ EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv(EMAIL_HOST_PASSWORD)
-EMAIL_USE_TLS = os.getenv(EMAIL_USE_TLS)
-DEFAULT_FROM_EMAIL = os.getenv(DEFAULT_FROM_EMAIL)
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
