a992dc97cef38332718e9c8dff1d87f63648979d
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-04-25 21:57:36 -0700

    aws

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -158,8 +158,8 @@ AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
 
