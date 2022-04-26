10f2a3783abc077e58f535f69dd35fd5f6bc3e82
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-04-25 19:22:28 -0700

    env

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -151,6 +151,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # media files configuration
 MEDIA_URL = '/media/'
