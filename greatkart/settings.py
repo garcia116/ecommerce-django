f8ff7fd7cbf7db0816e70bc97f0c033d7e415383
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-05-09 21:53:01 -0700

    database

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -101,17 +101,25 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ddqr816gs1959u',
-        'USER': 'gqxmhigxesyeuj',
-        'PASSWORD': '7845412139ad55e89901585dfcd53f8f9261fef046b5f0e5822970bb02e0261f',
-        'HOST': 'ec2-3-229-161-70.compute-1.amazonaws.com',
-        'PORT': 5432,
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
